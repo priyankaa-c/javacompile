f9dcd9eaff9ce37c58aa7e1033c2b4aba895ce95
Priyanka <priyankampatil151@gmail.com>
2026-02-01 23:49:11 +0530

    first commit

---
+++ sub.java
@@ -0,0 +1,9 @@
+public class sub {
+    public static void main(String[] args) {
+        int num1 = 50;
+        int num2 = 20;
+        int result = num1 - num2;
+        
+        System.out.println("Result: " + result);
+    }
+}
