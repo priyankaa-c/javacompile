7330f90d528d1e9354ad47693231b5129b0c3bac
priyankaa-c <155766880+priyankaa-c@users.noreply.github.com>
2026-02-03 16:59:13 +0530

    Change num2 value from 34 to 10

--- sub.java
+++ sub.java
@@ -1,7 +1,7 @@
 public class sub {
     public static void main(String[] args) {
         int num1 = 50;
-        int num2 = 34;
+        int num2 = 10;
         int result = num1 - num2;
         
         System.out.println("Result: " + result);
